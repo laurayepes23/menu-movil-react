29868aeec6f4fddd303659cc24ce8f4e1952bf1a
laurayepes23 <yepesorjuelalaurasofia@gmail.com>
2025-08-22 07:52:19 -0500

    actividad actualizada

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,13 +4,11 @@ export default function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="bg-blue-800 text-white p-4">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
-        {/* Logo */}
+    <nav className="bg-blue-800 text-white p-5">
+      <div className="max-w-8xl mx-auto flex justify-between items-center">
+        
         <h1 className="text-xl font-bold">Mi App</h1>
-
-        {/* Menú en pantallas grandes */}
-        <ul className="hidden md:flex gap-x-6">
+        <ul className="hidden md:flex gap-x-10">
           <li>
             <a href="#" className="hover:underline">
               Inicio
@@ -28,30 +26,29 @@ export default function Navbar() {
           </li>
         </ul>
 
-        {/* Botón Hamburguesa en pantallas pequeñas */}
+  
         <button
-          className="md:hidden text-2xl focus:outline-none"
+          className="md:hidden text-4xl focus:outline-none"
           onClick={() => setOpen(!open)}
         >
           {open ? "✕" : "☰"}
         </button>
       </div>
 
-      {/* Menú móvil */}
       {open && (
-        <ul className="md:hidden mt-4 flex flex-col gap-y-2">
+        <ul className="md:hidden mt-5 flex flex-col gap-y-5">
           <li>
-            <a href="#" className="hover:text-gray-200">
+            <a href="#" className="hover:text-gray-250">
               Inicio
             </a>
           </li>
           <li>
-            <a href="#" className="hover:text-gray-200">
+            <a href="#" className="hover:text-gray-250">
               Servicios
             </a>
           </li>
           <li>
-            <a href="#" className="hover:text-gray-200">
+            <a href="#" className="hover:text-gray-250">
               Contacto
             </a>
           </li>
